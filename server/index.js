8a2908afde6922685a46819dcff6da8dc4b1da92
Menchu MV <mcmvillalobos@gmail.com>
2021-11-17 18:31:48 +0100

    Added initializeServer function in server index

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const debug = require("debug")("tuitah:server");
+const chalk = require("chalk");
+const morgan = require("morgan");
+
+const app = express();
+
+const initializeServer = (port) =>
+  new Promise((resolve) => {
+    const server = app.listen(port, () => {
+      debug(chalk.green(`¡Escuchanddo en ${port}! ʕ•ᴥ•ʔﾉ♡`));
+      resolve(server);
+    });
+
+    server.on("error", (error) => {
+      debug(chalk.red("No se ha podido iniciar el servidor :("));
+      if (error.code === "EADDRINUSE") {
+        debug(chalk.red(`${port} está en uso...  ʕʘ̅┏ل͜┓ʘ̅ʔ`));
+      }
+    });
+  });
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+module.exports = { initializeServer, app };
